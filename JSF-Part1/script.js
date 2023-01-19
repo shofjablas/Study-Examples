982b7a34c156020676ef1af2e01a2b508974729c
shofjablas <shluxnsal01@gmail.com>
2023-01-20 01:25:48 +0900

    Data Types 공부

---
+++ JSF-Part1/script.js
@@ -0,0 +1,19 @@
+let country = "South Korea";
+let continent = "Asia";
+let population = 5174;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = "Jejudo";
+Jejudo = true;
+console.log(Jejudo);
+
+let language;
+console.log(language);
+
+console.log(typeof "SouthKorea");
+console.log(typeof "Asia");
+console.log(typeof 5174);
+console.log(typeof language);

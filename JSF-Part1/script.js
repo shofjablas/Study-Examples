d4efe3c94504703c6a466ac550ad6be2807a1ad4
shofjablas <shluxnsal01@gmail.com>
2023-01-21 23:08:30 +0900

    Basic Operators

--- JSF-Part1/script.js
+++ JSF-Part1/script.js
@@ -1,19 +1,46 @@
-let country = "South Korea";
-let continent = "Asia";
-let population = 5174;
+//Values and Variables
+// let country = "South Korea";
+// let continent = "Asia";
+let population = 51;
 
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
 console.log(population);
 
-let isIsland = "Jejudo";
-Jejudo = true;
-console.log(Jejudo);
+//Data Types
+// let isIsland = true;
+// console.log(isIsland);
 
 let language;
 console.log(language);
 
-console.log(typeof "SouthKorea");
-console.log(typeof "Asia");
-console.log(typeof 5174);
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
 console.log(typeof language);
+
+//let, const and var
+language = "Korean";
+console.log(language);
+
+const country = "South Korea";
+const continent = "Asia";
+const isIsland = true;
+
+//Basic Operators
+let populationHalf = population / 2;
+console.log(populationHalf);
+
+//2
+let populationPlusOne = population++;
+console.log(populationPlusOne);
+
+//3
+let populationFinland = 6;
+console.log(population > populationFinland);
+
+//4
+let populationAverage = 33;
+console.log(populationAverage > population);
+
+//5

0efc0996beb74e75480473043e16519274d2c6b7
shofjablas <shluxnsal01@gmail.com>
2023-01-31 16:36:34 +0900

    type conversion and coercion

--- JSF-Part1/script.js
+++ JSF-Part1/script.js
@@ -68,3 +68,9 @@ if (population > 33) {
 } else {
   console.log(`${country} population is ${33 - population} average`);
 }
+
+console.log("9" - "5");
+console.log("19" - "13" + "17");
+console.log("19" - "13" + 17);
+console.log("123" < 57);
+console.log(5 + 6 + "4" + 9 - 4 - 2);

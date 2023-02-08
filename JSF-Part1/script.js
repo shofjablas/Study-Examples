51e80f7f57cbf764eb4ce4719e5b58cbb9c375cd
shofjablas <shluxnsal01@gmail.com>
2023-02-08 23:56:41 +0900

    part1

--- JSF-Part1/script.js
+++ JSF-Part1/script.js
@@ -89,8 +89,39 @@ console.log(5 + 6 + "4" + 9 - 4 - 2);
 //   console.log("More than 1 border ");
 // } else console.log("No borders");
 
+//logical opertaors
 if (language === "english" && population < 50 && !isIsland) {
   console.log(`You should live in ${country} `);
 } else {
   console.log(`${country} does not meet your criteria`);
 }
+
+//the switch statement
+
+/*
+switch (language) {
+  case "chinese" && "mandarin":
+    console.log("Most number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great lanuage too :D");
+}
+*/
+
+//The Conditional (Ternary) Operator
+
+population >= 55
+  ? `${country} population is above average.`
+  : `${country} population is below average`;
